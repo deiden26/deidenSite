507ebdf29537fd1c0e97353154f007378f46e39c
Danny Eiden <deiden26@gmail.com>
2014-09-04 00:33:44 -0500

    fix topbar expansion hiding so it doesn't mess up full top bar formatting

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,7 @@
-function scrollTo(target){
+function scrollTo(target, closeNav){
 	$('html, body').animate({
 		scrollTop: $(target).offset().top
 	}, 500);
+	if(closeNav)
+		Foundation.libs.topbar.toggle();
 }
