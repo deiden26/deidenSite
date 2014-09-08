7fb9c47eb19e188cb588f7ff6fe925d615a781d0
Danny Eiden <deiden26@gmail.com>
2014-09-08 18:54:58 -0500

    add offset to scrollto code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 function scrollTo(target, closeNav){
 	$('html, body').animate({
-		scrollTop: $(target).offset().top
+		scrollTop: $(target).offset().top -45
 	}, 500);
 	if(closeNav)
 		Foundation.libs.topbar.toggle();
