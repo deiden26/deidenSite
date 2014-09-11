440c5fa156183497a478e2509aa52a1982174d40
Danny Eiden <deiden26@gmail.com>
2014-09-10 22:03:41 -0500

    add contact section with working links and icons

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 function scrollTo(target, closeNav){
 	$('html, body').animate({
-		scrollTop: $(target).offset().top -45
+		scrollTop: $(target).offset().top -40
 	}, 500);
 	if(closeNav)
 		Foundation.libs.topbar.toggle();
