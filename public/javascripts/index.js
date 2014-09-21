9eaeaa926a3149f1e5a81776d776c6cce2772ef1
Danny Eiden <deiden26@gmail.com>
2014-09-21 17:57:16 -0500

    center skills titles, comment javascript, add show/hide buttons for experience content

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,17 @@
 function scrollTo(target, closeNav){
+	//Scroll with animation
 	$('html, body').animate({
 		scrollTop: $(target).offset().top -40
 	}, 500);
+	//Close the nav bar menu
 	if(closeNav)
 		Foundation.libs.topbar.toggle();
+}
+function showExperienceContent(target){
+	//Show or hide the content
+	$("."+target+"-content").slideToggle(400);
+	//Add or remove the down arrow
+	$("#"+target+"-arrow").toggleClass("fa fa-angle-down fa-3x");
+	//Add or remove the up arrow
+	$("#"+target+"-arrow").toggleClass("fa fa-angle-up fa-3x");
 }
