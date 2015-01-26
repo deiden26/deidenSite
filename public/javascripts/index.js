09d0826c9d80b8e4d82f233f9c56cc0f61fc1475
Danny Eiden <deiden26@gmail.com>
2015-01-25 19:13:27 -0600

    Use FA icons for carousel. Move 'more info' button out of carousel.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 	$('.project-carousel').slick({
-		adaptiveHeight: true,
+		onBeforeChange: hideExperienceContent
 	});
 });
 
@@ -14,11 +14,35 @@ function scrollTo(target, closeNav){
 		Foundation.libs.topbar.toggle();
 }
 
-function showExperienceContent(target){
-	//Show or hide the content
-	$("."+target+"-content").slideToggle(400);
-	//Add or remove the down arrow
-	$("#"+target+"-arrow").toggleClass("fa fa-angle-down fa-3x");
-	//Add or remove the up arrow
-	$("#"+target+"-arrow").toggleClass("fa fa-angle-up fa-3x");
+function hideExperienceContent(){
+	toggleExperienceContent(false);
+}
+
+//showHide=true -> only show
+//showHide=false -> only hide
+//showHide=undefined -> toggle
+function toggleExperienceContent(showHide){
+	var contentLabels = ["divit", "shindg"];
+	var index = $(".project-carousel").slickCurrentSlide();
+	var target = contentLabels[index];
+	var content = $("."+target+"-content");
+	var contentButtonText = $(".content-button-text");
+	if (showHide === true && content.is(':visible'))
+		return;
+	else if (showHide === false && !content.is(':visible'))
+		return;
+	else {
+		//Show or hide the content
+		content.slideToggle(400);
+		//Add or remove the down arrow
+		$("#content-arrow").toggleClass("fa fa-angle-double-down fa-3x");
+		//Add or remove the up arrow
+		$("#content-arrow").toggleClass("fa fa-angle-double-up fa-3x");
+		//Change test
+		if(contentButtonText.text() === "More Info")
+			contentButtonText.html("Less Info");
+		else
+			contentButtonText.html("More Info");
+
+	}
 }
