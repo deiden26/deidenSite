da2aedb7fcd009d934840cfa12ba4e45198b52f5
Danny Eiden <deiden26@gmail.com>
2015-02-07 14:11:25 -0600

    update resume and have download name resume with current date

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	//Create date for resume download
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1;
+	var yyyy = today.getFullYear();
+	var todayString = mm + "-" + dd +"-" + yyyy;
+	res.render('index', { date: todayString });
 });
 
 module.exports = router;
