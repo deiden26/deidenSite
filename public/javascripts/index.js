c31b567b2d77c2d0262fca1173d5f1653df7f7f9
Danny Eiden <deiden26@gmail.com>
2015-02-08 14:27:05 -0600

    improve performance of scroll-fade animation and comment index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,33 @@
+//Cached global variables for scroll-fading
+var mobile = false;
+var topImageText;
+var topImageTextOpacity;
+//Initializer for when the page finishes loading
 document.addEventListener("DOMContentLoaded", function() {
+	//Initialize carousel
 	$('.project-carousel').slick({
 		dots: true,
 		appendDots: $(".dots-container"),
 		onBeforeChange: hideExperienceContent
 	});
+	//Initialize cached variables for scroll-fading
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		mobile = true;
+	}
+	topImageText = document.querySelector("#top-image-text")
 });
+//Response to scrolling that causes top image text to fade
 window.onscroll = function() {
-	if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		var alphaRaw = 1 - (2.5/window.innerHeight) * window.pageYOffset;
-		var alpha = Math.max(0, alphaRaw);
-		$(".top-image-text").css('opacity', alpha);
+	if(!mobile) {
+		topImageTextOpacity = 1 - (2.5/window.innerHeight) * window.pageYOffset;
+		window.requestAnimationFrame(fade);
 	}
 };
+//Fade function
+function fade() {
+	topImageText.style.opacity = topImageTextOpacity;
+};
+//Top bar button scrolling response
 function scrollTo(target, closeNav){
 	//Scroll with animation
 	$('html, body').animate({
@@ -21,11 +37,12 @@ function scrollTo(target, closeNav){
 	if(closeNav)
 		Foundation.libs.topbar.toggle();
 }
-
+//Hide the content of the "My contributions" div
 function hideExperienceContent(){
 	toggleExperienceContent(false);
 }
 
+//Toggle the content of the "My contributions" div
 //showHide=true -> only show
 //showHide=false -> only hide
 //showHide=undefined -> toggle
