fcce93ce6a1464aab05ce15b40b93666fa532cc1
Danny Eiden <deiden26@gmail.com>
2019-05-29 16:53:47 -0400

    Update Content
    * Remove old, extraneous content
    * Update personal statement, skill blurbs, and headshot
    * Use symbolically linked, separately managed resume
    * Remove unused libraries, JS, and styles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,17 +3,8 @@ var mobile = false;
 var topImageText;
 var topImageTextOpacity;
 
-// Global variable for carousel content labels
-var contentLabels = ['anser', 'divit', 'shindg'];
-
 // Initializer for when the page finishes loading
 document.addEventListener('DOMContentLoaded', function() {
-    // Initialize carousel
-    $('.project-carousel').slick({
-        dots: true,
-        appendDots: $('.dots-container'),
-        onBeforeChange: beforeSlideChange,
-    });
 
     // Disable fading and parralax for mobile
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
@@ -54,62 +45,5 @@ function scrollToSection(target, closeNav) {
     // Close the nav bar menu
     if (closeNav) {
         Foundation.libs.topbar.toggle();
-    }
-}
-
-function beforeSlideChange(slick, currentSlide, nextSlide) {
-    var currentSlideLabel = contentLabels[currentSlide];
-    var nextSlideLabel = contentLabels[nextSlide];
-    ga(
-        'send',
-        'event',
-        'Project Carousel - ' + currentSlideLabel,
-        'click',
-        'To - '+ nextSlideLabel
-    );
-    // Hide the content of the "My contributions" div
-    toggleExperienceContent(false);
-}
-
-// Toggle the content of the "My contributions" div
-// showHide=true -> only show
-// showHide=false -> only hide
-// showHide=undefined -> toggle
-function toggleExperienceContent(showHide) {
-    var index = $('.project-carousel').slickCurrentSlide();
-    var target = contentLabels[index];
-    var content = $('#' + target + '-content');
-    var contentButtonText = $('.content-button-text');
-    if ((showHide === true && content.is(':visible')) ||
-        (showHide === false && !content.is(':visible'))
-    ) {
-        return;
-    }
-
-    // Show or hide the content
-    content.slideToggle(400);
-    // Add or remove the down arrow
-    $('#content-arrow').toggleClass('fa fa-angle-double-down fa-3x');
-    // Add or remove the up arrow
-    $('#content-arrow').toggleClass('fa fa-angle-double-up fa-3x');
-    // Change test
-    if (contentButtonText.text() === 'Show My Contributions') {
-        contentButtonText.html('Hide My Contributions');
-        ga(
-            'send',
-            'event',
-            'Project Contributions - ' + taget,
-            'click',
-            'Show'
-        );
-    } else {
-        contentButtonText.html('Show My Contributions');
-        ga(
-            'send',
-            'event',
-            'Project Contributions - ' + target,
-            'click',
-            'Hide'
-        );
     }
 }
